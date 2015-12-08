e8961e4775f4c819e1929009b4b26746f8dbd974
root <root@62-210-99-62.online.net>
2015-12-08 19:00:33 +0100

    add csv and proper parsing

--- Spark.scala
+++ Spark.scala
@@ -3,6 +3,11 @@ import org.apache.spark.rdd
 import org.apache.spark.rdd._
 import org.apache.spark._
 import org.apache.spark.sql.SQLContext._
+import org.apache.spark.sql._
+import org.joda.time.DateTime
+import org.joda.time.format.{ DateTimeFormat, DateTimeFormatter }
+import com.github.tototoshi.csv._
+import java.io.{File, FileOutputStream}
 /**
   * Created by Louis on 02/12/2015.
   */
@@ -15,21 +20,35 @@ import org.apache.spark.sql.SQLContext._
 	 
          
 	 case class CrimeAverage(name: String, value: Float)
+     
 
+     val file = sc.textFile("SacramentocrimeJanuary2006.csv").mapPartitionsWithIndex{ (idx, iter) => if(idx == 0) iter.drop(1) else iter}
 
-         val file = sc.textFile("SacramentocrimeJanuary2006.csv").mapPartitionsWithIndex{ (idx, iter) => if(idx == 0) iter.drop(1) else iter}
-	 val crimeTypes = file.map (line => { 
+	 	val crimeTypes = file.map (line => { 
 						val l = line.split(",")
 						l(5)
 					})
         val crimeDate = file.map(line => {
 						val l = line.split(",")
-						l(0).substring(0,9)
+						val dateTime = DateTimeFormat.forPattern("M/d/yy H:mm").parseDateTime(l(0))						
+						val out = DateTimeFormat.forPattern("dd/MM/yy")
+						out.print(dateTime)
 				})
+
+        val crimeDistrictDay = file.map(line => {
+						val l = line.split(",")
+						val dateTime = DateTimeFormat.forPattern("M/d/yy H:mm").parseDateTime(l(0))						
+						val out = DateTimeFormat.forPattern("dd/MM/yy")
+						out.print(dateTime)+" District  "+l(2)
+				})
+
+        
         val threeHighestCrimeDays = crimeDate.groupBy(w => w).mapValues(_.size).takeOrdered(3)(Ordering[Int].reverse.on(_._2))
     	val mostRecurrentCrime  = crimeTypes.groupBy(w => w).mapValues(_.size).takeOrdered(1)(Ordering[Int].reverse.on(_._2))
-	val crimeAveragePerDay = crimeTypes.groupBy(w => w).mapValues(_.size).map(item => CrimeAverage(item._1,(item._2.toFloat)/31)).sortBy(_.value)
-	       
+	    val crimeAveragePerDay = crimeTypes.groupBy(w => w).mapValues(_.size).map(item => CrimeAverage(item._1,(item._2.toFloat)/31)).sortBy(_.value)
+
+	    threeHighestCrimeDays.foreach(println)
+	      
 
      
 	println("Most recurrent crime is :")
@@ -46,14 +65,16 @@ import org.apache.spark.sql.SQLContext._
     val threeHighestCrimeDaysDF = crimeDateDF.groupBy('_1).count().sort('count.desc).take(3)
     val crimeAveragePerDayDF = crimeTypesDF.groupBy('_1).count().select('_1,'count /31f)
 
-
     println("Most recurrent crime is :")
     println(mostReccurentCrimeDF)
     println("The three days with highest crime rate are:")
     threeHighestCrimeDaysDF.foreach(println)
     println("Crime average per day")
     crimeAveragePerDayDF.foreach(println)
+ 
+     val crimePerDistrictPerDay = crimeDistrictDay.groupBy(w => w).mapValues(_.size).sortByKey()
+     crimePerDistrictPerDay.foreach(println)
     
+     crimePerDistrictPerDay.map(a => a._1 + "," + a._2).saveAsTextFile("out.csv")
   }
 }
-
--- build.sbt
+++ build.sbt
@@ -7,3 +7,7 @@ scalaVersion := "2.11.7"
 libraryDependencies +=  "org.apache.spark" %% "spark-core" % "1.5.2"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.5.2"
+
+libraryDependencies += "joda-time" % "joda-time" % "2.9.1"
+
+libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.2.2"
